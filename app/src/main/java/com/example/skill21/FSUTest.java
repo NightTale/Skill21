c592927d6e0ca705d784c68cb2de4feaafb36c77
NightTale <artempotexin95@gmail.com>
2024-10-25 18:00:52 +0300

    1.1

--- app/src/main/java/com/example/skill21/FSUTest.java
+++ app/src/main/java/com/example/skill21/FSUTest.java
@@ -1,8 +1,6 @@
 package com.example.skill21;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class FSULesson1 extends AppCompatActivity {
+public class FSUTest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        );
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_fsulesson1);
+        setContentView(R.layout.activity_fsutest);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void toFSUTest(View v) {
-        Intent intent;
-        intent = new Intent(this, FSUTest.class);
-        startActivity(intent);
-    }
-    public void toCard1(View v) {
-        Intent intent;
-        intent = new Intent(this, Card1Activity.class);
-        startActivity(intent);
     }
 }
